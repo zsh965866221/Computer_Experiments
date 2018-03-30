5624cd96b4517d863cd088d96e58af6e21c5b830
zsh965866221 <965866221@qq.com>
2018-03-30 09:18:26 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Computer_Experiments.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Computer_Experiments.dir/main.cpp.obj"
+  "Computer_Experiments.pdb"
+  "Computer_Experiments.exe"
+  "Computer_Experiments.exe.manifest"
+  "libComputer_Experiments.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Computer_Experiments.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Computer_Experiments.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Computer_Experiments.
+# This may be replaced when dependencies are built.
